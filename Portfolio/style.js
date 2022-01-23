f2fb899f5e244acff65bcd09b06a513da19dca38
Mrt-GitHub <bern.murat@gmail.com>
2022-01-23 18:04:31 +0100

    This files added For Powercoders additional task

---
+++ Portfolio/style.js
@@ -0,0 +1,6 @@
+function changeColor() {
+    let color_paragraph = document.getElementById('color_paragraph');
+    let color_input = document.getElementById('color_input');
+    let color = color_input.value;
+    color_paragraph.style.color = color;
+  } 
